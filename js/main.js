76432ef9840bace645300aa4d0f8e1e664c4e068
khansaaftab85-design <khansaaftab85@gmail.com>
2026-07-24 22:49:26 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -106,4 +106,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
         observer.observe(statsSection);
     }
+    const backToTopBtn = document.getElementById("backToTop");
+
+  if (backToTopBtn) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        backToTopBtn.classList.add("show");
+      } else {
+        backToTopBtn.classList.remove("show");
+      }
+    });
+
+    backToTopBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  }
 });
